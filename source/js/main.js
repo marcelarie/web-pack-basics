8bb84c3f49a64f19741d8849462e788d88b399f6
Marcel Arie <googlillo@gmail.com>
2021-03-11 15:20:32 +0100

    Add webpack base config and fix js

--- source/js/main.js
+++ source/js/main.js
@@ -1,2 +1,10 @@
+import {images} from './module-b.js';
 
+const render = (template) => {
+    document.getElementById('images').insertAdjacentHTML('beforeend', template);
+}
 
+render(images.mario.returnTemplate());
+render(images.mushroom.returnTemplate());
+render(images.logo.returnTemplate());
+render(images.wallpaper.returnTemplate());
--- source/js/module-b.js
+++ source/js/module-b.js
@@ -1,16 +1,11 @@
 import {ImageGenerator} from './module-a.js';
 
+export const images = {
+    mario: new ImageGenerator('50px', '../public/img/mario.png'),
+    mushroom: new ImageGenerator('100px', '../public/img/mushroom.png'),
+    logo: new ImageGenerator('150px', '../public/img/LOGO.svg'),
+    wallpaper: new ImageGenerator('300px', '../public/img/wallpaper.jpg')
+}
 
-const image01 = new ImageGenerator('100px', '../public/img/mario.png');
-const image02 = new ImageGenerator('100px', '../public/img/mushroom.png');
-const image03 = new ImageGenerator('100px', '../public/img/LOGO.svg');
-const image04 = new ImageGenerator('100px', '../public/img/wallpaper.jpg');
-
-
-
-document.getElementById('images').insertAdjacentHTML('beforeend', image01.returnTemplate());
-document.getElementById('images').insertAdjacentHTML('beforeend', image02.returnTemplate());
-document.getElementById('images').insertAdjacentHTML('beforeend', image03.returnTemplate());
-document.getElementById('images').insertAdjacentHTML('beforeend', image04.returnTemplate());
 
 
